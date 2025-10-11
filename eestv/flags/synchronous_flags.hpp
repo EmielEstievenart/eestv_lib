7f3621211124fdbfd13f05402d4e317b12a8a861
Emiel Estiévenart <emiel-perijah@hotmail.com>
2025-10-11 14:17:34 +0200

    Remove mutex from flags
    (cherry picked from commit 4aab17e0f9a776e44ccbd49204ad0e81320f7b2e)

--- eestv/flags/synchronous_flags.hpp
+++ eestv/flags/synchronous_flags.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <cstdint>
-#include <mutex>
 #include <type_traits>
 
 namespace eestv
@@ -19,54 +18,33 @@ public:
      * Set a specific flag bit
      * @param flag The flag to set (enum value will be converted to bit position)
      */
-    void set_flag(FlagType flag)
-    {
-        std::lock_guard<std::mutex> lock(_mutex);
-        _flags |= (1U << static_cast<uint32_t>(flag));
-    }
+    void set_flag(FlagType flag) { _flags |= (1U << static_cast<uint32_t>(flag)); }
 
     /**
      * Clear a specific flag bit
      * @param flag The flag to clear (enum value will be converted to bit position)
      */
-    void clear_flag(FlagType flag)
-    {
-        std::lock_guard<std::mutex> lock(_mutex);
-        _flags &= ~(1U << static_cast<uint32_t>(flag));
-    }
+    void clear_flag(FlagType flag) { _flags &= ~(1U << static_cast<uint32_t>(flag)); }
 
     /**
      * Get the state of a specific flag bit
      * @param flag The flag to check (enum value will be converted to bit position)
      * @return true if the flag bit is set, false otherwise
      */
-    bool get_flag(FlagType flag) const
-    {
-        std::lock_guard<std::mutex> lock(_mutex);
-        return (_flags & (1U << static_cast<uint32_t>(flag))) != 0;
-    }
+    bool get_flag(FlagType flag) const { return (_flags & (1U << static_cast<uint32_t>(flag))) != 0; }
 
     /**
      * Clear all flags
      */
-    void clear_all()
-    {
-        std::lock_guard<std::mutex> lock(_mutex);
-        _flags = 0;
-    }
+    void clear_all() { _flags = 0; }
 
     /**
      * Get the raw flags value
      * @return The current flags as a uint32_t
      */
-    uint32_t get_raw() const
-    {
-        std::lock_guard<std::mutex> lock(_mutex);
-        return _flags;
-    }
+    uint32_t get_raw() const { return _flags; }
 
 private:
     uint32_t _flags = 0;
-    mutable std::mutex _mutex;
 };
 } // namespace eestv
