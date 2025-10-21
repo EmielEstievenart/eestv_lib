12d2afb89a7fa58a4191cab778bb67eeb6fd2536
Emiel Estiévenart <emiel-perijah@hotmail.com>
2025-10-21 19:39:49 +0200

    Fix thread safety of tpc_server

--- eestv/net/connection/tcp_server_states.hpp
+++ eestv/net/connection/tcp_server_states.hpp
@@ -5,18 +5,28 @@ namespace eestv
 
 enum class TcpServerState
 {
+    start_signaled,
+    stop_signaled,
+    starting_accept,
     accepting,
-    closing
+    stopping
+
 };
 
 inline const char* to_string(TcpServerState state) noexcept
 {
     switch (state)
     {
+    case TcpServerState::start_signaled:
+        return "start_signaled";
+    case TcpServerState::stop_signaled:
+        return "stop_signaled";
+    case TcpServerState::starting_accept:
+        return "starting_accept";
     case TcpServerState::accepting:
         return "accepting";
-    case TcpServerState::closing:
-        return "closing";
+    case TcpServerState::stopping:
+        return "stopping";
     }
     return "unknown";
 }
